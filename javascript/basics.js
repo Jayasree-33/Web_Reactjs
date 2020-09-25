7e4030495a1560e56886ea3b66c638f930217287
Jayasree-33 <jayasreepotnuru93@gmail.com>
2020-09-25 19:08:43 +0530

    project

---
+++ javascript/basics.js
@@ -0,0 +1,9 @@
+function display1(){
+	alert("javascript in the external file");
+	}
+var x=5;
+var 123="jaya"; //wrong declaration
+var $jaya=256;
+var _jaya=jaya2564;
+var jaya_125=6841;
+var 2ja=561; //wrong declaration
